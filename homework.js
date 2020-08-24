03d57a9b14bfd6b9eaae645b6f63bba0ac8452ad
luzyla <luzyla@gmail.com>
2020-08-24 00:31:29 -0300

    Ejercitación finalizada

--- homework.js
+++ homework.js
@@ -169,7 +169,7 @@ console.log(puedeRenovarCarnet(false, false, false)) // false
 // -----------------------------------------
 // puedeGraduarse(asistencia, materiasAprobadas, tesisAprobada)
 // -----------------------------------------
-console.log("Ejercicio 7: puedeGraduarse(asistencia, materiasAprobadas, tesisAprobada)")
+console.log("Ejercicio 7: puedeGraduarse")
 
 const puedeGraduarse = (asistencia, materiasAprobadas, tesisAprobada) => {
 
@@ -195,4 +195,227 @@ console.log(puedeGraduarse(33, 55, false)) // false
 console.log(puedeGraduarse(42, 45, true))  // false
 console.log(puedeGraduarse(28, 45, false)) // false
 
+// -----------------------------------------
+// esParOImpar(numero)
+// -----------------------------------------
+console.log("Ejercicio 8: esParOImpar")
 
+const esParOImpar = (numero) => {
+
+    if((numero %= 2) == 0) {
+
+        return "par"
+    }
+
+    else {
+
+        return "impar"
+    }
+}
+
+console.log(esParOImpar(3))  // 'impar'
+console.log(esParOImpar(10)) // 'par'
+
+// -----------------------------------------
+//  esPositivoONegativo(numero)
+// -----------------------------------------
+
+console.log("Ejercicio 9: esPositivoONegativo")
+
+const esPositivoONegativo = (numero) => {
+
+    if (numero >= 0) {
+
+        return "positivo"
+
+    }
+
+    else {
+
+        return "negativo"
+
+    }
+
+}
+
+console.log(esPositivoONegativo(3))  // 'positivo'
+console.log(esPositivoONegativo(-5)) // 'negativo'
+
+
+// -----------------------------------------
+//  avanzarSemaforo(colorActual)
+// -----------------------------------------
+console.log("Ejercicio 10: avanzarSemaforo")
+
+const avanzarSemaforo = (colorActual) => {
+
+    if(colorActual == "verde") {
+
+        return "amarillo"
+
+    }
+
+    else if(colorActual == "amarillo") {
+        
+        return "rojo"
+
+    }
+
+    else {
+
+        return "verde"
+    }
+}
+
+console.log(avanzarSemaforo('verde'))     // 'amarillo'
+console.log(avanzarSemaforo('amarillo'))  // 'rojo'
+console.log(avanzarSemaforo('rojo'))      // 'verde'
+
+// -----------------------------------------
+//  obtenerSensacion(temperatura)
+// -----------------------------------------
+console.log("Ejercicio 11: obtenerSensacion")
+
+const obtenerSensacion = (temperatura) => {
+
+    if (temperatura < 0) {
+
+        return "¡Está helando!"
+    }
+
+    else if (0 <= temperatura && temperatura < 15) {
+
+        return "¡Hace frío!"
+
+    }
+
+    else if (15 <= temperatura && temperatura < 25) {
+
+        return "Está lindo"
+    }
+
+    else if (25 <= temperatura && temperatura < 30) {
+
+        return "Hace calor"
+    }
+
+    else {
+
+        return "¡Hace mucho calor!"
+
+    }
+}
+
+console.log(obtenerSensacion(33)) // "¡Hace mucho calor!"
+console.log(obtenerSensacion(28)) // "Hace calor"
+console.log(obtenerSensacion(20)) // "Está lindo"
+console.log(obtenerSensacion(10)) // "¡Hace frío!"
+console.log(obtenerSensacion(-2)) // "¡Está helando!"
+
+// -----------------------------------------
+//  obtenerNota(puntaje)
+// -----------------------------------------
+console.log("Ejercicio 12: obtenerNota")
+
+const obtenerNota = (puntaje) => {
+
+    if (puntaje < 6) {
+
+        return "Desaprobado :("
+    }
+
+    else if (6 <= puntaje && puntaje < 7) {
+
+        return "Regular"
+    }
+
+    else if (7 <= puntaje && puntaje < 8) {
+
+        return "Bueno"
+    }
+
+    else if (8 <= puntaje && puntaje < 10) {
+
+        return "Muy Bueno"
+    }
+
+    else if (puntaje == 10) {
+
+        return "Excelente! :D"
+    }
+
+    else {
+
+        return "Puntaje inválido"
+    }
+
+}
+
+console.log(obtenerNota(7))    // "Bueno"
+console.log(obtenerNota(9.6))  // "Excelente" <-- no! "Muy bueno" Excelente es 10 solamente
+console.log(obtenerNota(12))   // "Puntaje inválido"
+
+// -----------------------------------------
+// jugarPiedraPapelTijera(a, b)
+// -----------------------------------------
+console.log("Ejercicio 13: jugarPiedraPapelTijera")
+
+const jugarPiedraPapelTijera = (a, b) => {
+
+    if (a == 'piedra') {
+        if (b == 'papel') {
+            return "¡Ganó papel!"
+        }
+
+        else if (b == 'tijera') {
+            return "¡Ganó piedra!"
+        }
+        
+        else {
+             return "Empate"
+        }
+    }
+
+    if (a == 'papel') {
+        if (b == 'piedra') {
+            return "¡Ganó papel!"
+        }
+
+        else if (b == 'tijera') {
+            return "¡Ganó tijera!"
+        }
+        
+        else {
+             return "Empate"
+        }
+    }
+
+    if (a == 'tijera') {
+        if (b == 'piedra') {
+            return "¡Ganó piedra!"
+        }
+
+        else if (b == 'papel') {
+            return "¡Ganó tijera!"
+        }
+        
+        else {
+             return "Empate"
+        }
+    }
+
+}
+
+console.log(jugarPiedraPapelTijera('tijera', 'piedra'))  // ¡Ganó piedra!
+console.log(jugarPiedraPapelTijera('piedra', 'tijera'))  // ¡Ganó piedra!
+console.log(jugarPiedraPapelTijera('papel', 'piedra'))   // ¡Ganó papel!
+console.log(jugarPiedraPapelTijera('piedra', 'papel'))   // ¡Ganó papel!
+console.log(jugarPiedraPapelTijera('papel', 'tijera'))   // ¡Ganó tijera!
+console.log(jugarPiedraPapelTijera('tijera', 'papel'))   // ¡Ganó tijera!
+console.log(jugarPiedraPapelTijera('piedra', 'piedra'))  // ¡Empate!
+console.log(jugarPiedraPapelTijera('papel', 'papel'))    // ¡Empate!
+console.log(jugarPiedraPapelTijera('tijera', 'tijera'))  // ¡Empate!
+
+// -----------------------------------------
+//              @Luzyla
+// -----------------------------------------
